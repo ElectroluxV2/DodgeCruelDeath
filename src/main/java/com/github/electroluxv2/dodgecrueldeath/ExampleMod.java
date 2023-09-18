a1801bae98891424f5ab85da75bf39caedad444e
Mateusz Budzisz <electroluxv2@gmail.com>
2023-09-18 20:37:23 +0200

    Initial commit

---
+++ src/main/java/com/github/electroluxv2/dodgecrueldeath/ExampleMod.java
@@ -0,0 +1,18 @@
+package com.github.electroluxv2.dodgecrueldeath;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements DedicatedServerModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("DodgeCruelDeath");
+
+	@Override
+	public void onInitializeServer() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
