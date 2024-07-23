e2c8e6b38b7681aeef7f9aee0ec3c42f98b1002d
Mateusz Budzisz <electroluxv2@gmail.com>
2024-07-23 22:33:58 +0200

    1.21.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 1.1.0
+- Updated to 1.21
+## 1.0.0
+- Initial release
+- 
--- src/main/java/com/github/electroluxv2/dodgecrueldeath/mixin/PlayerEntityMixin.java
+++ src/main/java/com/github/electroluxv2/dodgecrueldeath/mixin/PlayerEntityMixin.java
@@ -1,29 +1,29 @@
 package com.github.electroluxv2.dodgecrueldeath.mixin;
 
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(Player.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
-    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
     }
 
-    @Inject(method = "dropItem(Lnet/minecraft/item/ItemStack;ZZ)Lnet/minecraft/entity/ItemEntity;", at = @At("RETURN"))
+    @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;ZZ)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("RETURN"))
     protected void onDropItem(final ItemStack stack, final boolean throwRandomly, final boolean retainOwnership, final CallbackInfoReturnable<ItemEntity> cir) {
         if (this.isAlive()) return;
 
         final var entity = (ItemEntityAccessor) cir.getReturnValue();
         if (entity == null) return;
 
-        entity.setItemAge(Short.MIN_VALUE + 1); // Minecraft checks if age is equal to MIN_VALUE for entities to live infinitely
+        entity.setAge(Short.MIN_VALUE + 1); // Minecraft checks if age is equal to MIN_VALUE for entities to live infinitely
     }
 }
